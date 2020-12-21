ee6859d79eec37eb3244d98043672bd5397395a7
root <root@localhost.localdomain>
2020-12-21 03:05:01 -0500

    init

---
+++ config/redis.go
@@ -0,0 +1,3 @@
+package config
+
+const RedisConfig = `{"code":200,"data":[{"configId":114,"createTime":1585746957000,"environmentId":1,"fileName":"redis","id":9788,"key":"Default.password","project":"Golang_Redis","secure":false,"updateTime":1585746957000,"value":"","version":"master"},{"configId":114,"createTime":1585746915000,"environmentId":1,"fileName":"redis","id":9787,"key":"Default.addrs","project":"Golang_Redis","secure":false,"updateTime":1585746915000,"value":"192.168.6.3:6379","version":"master"}]}`
